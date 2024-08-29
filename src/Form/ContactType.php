4368496cdd84a8d67bdc7143825fb3b8e270d785
Robert Natkay <natkay.robert@nrcode.com>
2024-08-30 00:06:14 +0200

    Tel type

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,23 +14,21 @@ class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $fields = [
-            'first_name' => 'Keresztnév',
-            'last_name' => 'Vezetéknév',
-            'phone' => 'Telefonszám',
-            'email' => 'Email cím',
-        ];
-
-        foreach ($fields as $fieldName => $label) {
-            $builder->add($fieldName, null, [
+        $opt = function (string $placeholder): array {
+            return [
                 'label' => false,
                 'attr' => [
-                    'placeholder' => $label,
+                    'placeholder' => $placeholder,
                 ],
-            ]);
-        }
+            ];
+        };
 
-        $builder->add('save', SubmitType::class, ['label' => 'Hozzáadás']);
+        $builder
+            ->add('first_name', null, $opt('Keresztnév'))
+            ->add('last_name', null, $opt('Vezetéknév'))
+            ->add('phone', TelType::class, $opt('Telefonszám'))
+            ->add('email', EmailType::class, $opt('Email cím'))
+            ->add('save', SubmitType::class, ['label' => 'Hozzáadás']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
