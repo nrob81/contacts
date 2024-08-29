81f4165a7d59dfb3de1bbbd8c44a7ae271c163e8
Robert Natkay <natkay.robert@nrcode.com>
2024-08-29 13:00:06 +0200

    install phpstan, php-cs-fixer, fix importmap

--- importmap.php
+++ importmap.php
@@ -20,4 +20,13 @@ return [
         'version' => '1.11.3',
         'type' => 'css',
     ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '7.3.0',
+    ],
 ];
